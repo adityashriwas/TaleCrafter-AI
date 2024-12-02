0c62008b8ab7c1a504900d42d88ed0ff7e7395a9
Aditya Shriwas <adityashriwas09@gmail.com>
2024-12-02 19:13:25 +0530

    Added text to speech functionality

--- app/view-story/[id]/page.tsx
+++ app/view-story/[id]/page.tsx
@@ -2,16 +2,22 @@
 import { db } from "@/config/config";
 import { StoryData } from "@/config/schema";
 import { eq } from "drizzle-orm";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HTMLFlipBook from "react-pageflip";
 import BookCoverPage from "../_components/BookCoverPage";
 import StoryPages from "../_components/StoryPages";
 import LastPage from "../_components/LastPage";
+import { Button } from "@nextui-org/button";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
 
 function ViewStory({ params }: any) {
+  
+  const bookRef = useRef();
   const [story, setStory] = useState<any>();
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    console.log(params.id);
+    // console.log(params.id);
     getStory();
   }, []);
 
@@ -24,12 +30,16 @@ function ViewStory({ params }: any) {
     setStory(result[0]);
   };
   return (
-    <div className="p-10 md:px-20 lg:px-40">
+    <div className="p-10 md:px-20 lg:px-40 flex items-center bg-[#0C0414] justify-evenly flex-col">
       <h2 className="font-bold text-4xl text-center p-10 bg-primary text-white">
         {story?.output?.title}
       </h2>
       {/* @ts-ignore */}
-      <HTMLFlipBook showCover={true} className="mt-10" width={500} height={500}>
+      <HTMLFlipBook className="mt-10" width={500} height={500}
+      showCover={true}
+      useMouseEvents={false}
+      ref={bookRef}
+      >
         <div>
           <BookCoverPage imageUrl={story?.coverImage} />
         </div>
@@ -39,11 +49,27 @@ function ViewStory({ params }: any) {
               <StoryPages storyChapter={story?.output?.chapters[index]}/>
             </div>
           ))
-        }
-        {/* <div>
-          <LastPage/>
-        </div> */}
+        }        
       </HTMLFlipBook>
+        { count!=0 && <div className="absolute left-[40%] bottom-4"
+        onClick={()=>{
+          // @ts-ignore
+          bookRef.current.pageFlip().flipPrev();
+          setCount(count-1);
+        }}
+        >
+        <IoIosArrowDropleftCircle className="text-4xl cursor-pointer"/>
+        </div>}
+      
+        { count != (story?.output.chapters?.length-1) && <div className="absolute right-[40%] bottom-4" 
+        onClick={()=>{
+          // @ts-ignore
+          bookRef.current.pageFlip().flipNext();
+          setCount(count+1);
+        }}
+        >
+        <IoIosArrowDroprightCircle className="text-4xl cursor-pointer"/>
+        </div>}
     </div>
   );
 }
