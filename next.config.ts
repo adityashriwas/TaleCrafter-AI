afa8d61f668640b23393adb702908682896cf9c4
Aditya Shriwas <adityashriwas09@gmail.com>
2025-05-05 12:04:33 +0530

    added google analytics

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    domains: ['image.pollinations.ai']
-  }
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    domains: ['image.pollinations.ai'],
+  },
 };
 
 export default nextConfig;
